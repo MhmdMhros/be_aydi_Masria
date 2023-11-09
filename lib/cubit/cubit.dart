94fa74e2acbdb54898de62bbca66cc2f5225b5fb
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2023-11-09 13:02:01 +0200

    hel

--- lib/cubit/cubit.dart
+++ lib/cubit/cubit.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names, avoid_print
+
 import 'package:be_aydi_masria/cubit/state.dart';
 import 'package:be_aydi_masria/layouts/homepage.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
